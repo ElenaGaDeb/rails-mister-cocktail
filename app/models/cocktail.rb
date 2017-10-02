cfd2799f8dd21801f3bbd3421e7c31adf0b0fa92
elena garre debies <elena.garredebies@edhec.com>
2017-10-02 18:26:21 +0200

    2

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,7 +1,9 @@
 class Cocktail < ApplicationRecord
-  has_many :doses
-  has_many :ingredients #through: :doses
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
 
   validates :name, uniqueness: true
   validates :name, presence: true
+  #validates :ingredient, uniqueness: { scope: :cocktail}
+
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ApplicationRecord
   has_many :doses
-  has_many :cocktails through: :doses
+  has_many :cocktails
 
   validates :name, uniqueness: true
   validates :name, presence: true
